ba385824e3897b4621c088b0d5e58c377f64efd6
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-06-10 10:25:41 +0200

    Calling GET Post

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { Post } from '../post.model';
 import { NgForm } from '@angular/forms';
+import { PostsService } from '../post-list/posts.service';
 
 @Component({
   selector: 'app-post-create',
@@ -10,7 +11,8 @@ import { NgForm } from '@angular/forms';
 export class PostCreateComponent {
   enteredTitle = '';
   enteredContent = '';
-  @Output() postCreated = new EventEmitter();
+
+  constructor(public postsService: PostsService) {}
 
   onAddPost(form: NgForm) {
     if (form.invalid) {
@@ -20,6 +22,6 @@ export class PostCreateComponent {
       title: form.value.title,
       content: form.value.content,
     };
-    this.postCreated.emit(post);
+    this.postsService.addPost(form.value.title, form.value.content);
   }
 }
--- src/app/posts/post-list/posts.service.ts
+++ src/app/posts/post-list/posts.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Post } from '../post.model';
+import { Subject } from 'rxjs';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class PostsService {
   private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
 
   getPosts() {
     return [...this.posts];
@@ -12,5 +14,10 @@ export class PostsService {
   addPost(title: string, content: string) {
     const post: Post = { title: title, content: content };
     this.posts.push(post);
+    this.postsUpdated.next([...this.posts]);
+  }
+
+  getPostUpdateListener() {
+    return this.postsUpdated.asObservable();
   }
 }
