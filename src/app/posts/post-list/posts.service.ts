9a476a3debb25798d75d9afcc66f6005c406a464
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-06-17 11:45:12 +0200

    Adding Post method

--- src/app/posts/post-list/posts.service.ts
+++ src/app/posts/post-list/posts.service.ts
@@ -11,16 +11,25 @@ export class PostsService {
   constructor(private http: HttpClient) {}
 
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts').subscribe((postData) => {
-      this.posts = postData.posts;
-      this.postsUpdated.next([...this.posts]);
-    });
+    this.http
+      .get<{ message: string; posts: Post[] }>(
+        'http://localhost:3000/api/posts'
+      )
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    this.http
+      .post<{ message: string }>('http://localhost:3000/api/posts', post)
+      .subscribe((responseData) => {
+        console.log(responseData.message);
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
   getPostUpdateListener() {
