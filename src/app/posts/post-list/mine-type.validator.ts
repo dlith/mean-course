2c52e98ea5b864947d19fa124c51bab54becadec
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-07-18 16:39:41 +0200

    Starting with the Mine-Type Validator

---
+++ src/app/posts/post-list/mine-type.validator.ts
@@ -0,0 +1,18 @@
+import { AbstractControl } from '@angular/forms';
+import { Observable, Observer } from 'rxjs';
+
+export const mineType = (
+  control: AbstractControl
+): Promise<{ [key: string]: any }> | Observable<{ [key: string]: any }> => {
+  const file = control.value as File;
+  const fileReader = new FileReader();
+  const frObs = Observable.create(
+    (observer: Observer<{ [key: string]: any }>) => {
+      fileReader.addEventListener('loadend', () => {
+
+      });
+    }
+  );
+  fileReader.readAsArrayBuffer(file);
+  return null;
+};
