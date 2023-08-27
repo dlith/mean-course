5ee160492a9731c14bbd843683051c6421ad6e2b
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-08-27 12:23:01 +0200

    Connecting Angular to the Backend

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -9,7 +10,14 @@ import { NgForm } from '@angular/forms';
 export class SignupComponent {
   isLoading = false;
 
+  constructor(public authService: AuthService) {}
+
   onSignup(form: NgForm) {
-    console.log(form.value);
+
+    if(form.invalid) {
+      return;
+    } else {
+      this.authService.createUser(form.value.email, form.value.password);
+    }
   }
 }
