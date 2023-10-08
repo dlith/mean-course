9cd161d0bb734dc0708facc7c926af11e660e723
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-10-08 10:29:55 +0200

    Sending the Token to the Frontend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -24,23 +24,20 @@ router.post("/signup", (req, res, next) => {
         res.status(500).json({
           error: err
         });
-    });
+      });
   });
 });
 
 router.post("/login", (req, res, next) => {
+  let fetchedUser;
   User.findOne({ email: req.body.email })
     .then(user => {
       if (!user) {
         return res.status(401).json({
           message: "Auth failed"
         });
-      } 
-      const token = jwt.sign(
-        { email: user.email, userId: user._id }, 
-        'secret_this_should_be_longer_only_for_test', 
-        { expiresIn: "1h" }
-      );
+      }
+      fetchedUser = user;
       return bcrypt.compare(req.body.password, user.password);
     })
     .then(result => {
@@ -49,12 +46,20 @@ router.post("/login", (req, res, next) => {
           message: "Auth failed"
         });
       }
+      const token = jwt.sign(
+        { email: fetchedUser.email, userId: fetchedUser._id },
+        "secret_this_should_be_longer",
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({
+        token: token
+      });
     })
     .catch(err => {
       return res.status(401).json({
         message: "Auth failed"
       });
-    })
+    });
 });
 
 
