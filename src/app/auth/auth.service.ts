fd4f7eb47e994b916aba81e2ec3570d2c6cd78f5
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2024-01-06 11:30:40 +0100

    Fixing an Update Bug

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -125,7 +125,6 @@ export class AuthService {
   }
 
   private setAuthTimer(duration: number) {
-    console.log("Setting timer: " + duration);
     this.tokenTimer = setTimeout(() => {
       this.logout();
     }, duration * 1000);
